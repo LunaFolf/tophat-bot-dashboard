1d98e98f680cf7aa5a2ae80a0fd0636e03baf99d
WhiskeeDev <email@liamholmes.co.uk>
2020-11-17 20:41:09 +0000

    new api module

--- src/api/http.js
+++ src/api/http.js
@@ -1,22 +1,47 @@
-import axios from 'axios'
-
 const apiBaseUrl = process.env.VUE_APP_jax_api_url
-const clientToken = process.env.VUE_APP_jax_client_token
+// const clientToken = process.env.VUE_APP_jax_client_token
 
 const api = {
   URL (url) {
-    return url.startsWith('http') ? url : apiBaseUrl + url
+    console.log(url)
+    return new URL(url.startsWith('http') ? url : apiBaseUrl + url)
   },
   get (url, options = {}) {
     var headers = options.headers || {}
-    if (api.URL(url).includes(apiBaseUrl)) headers['jax-client-token'] = clientToken
-    return axios.get(api.URL(url), { headers })
+    return httpRequest(api.URL(url), { headers })
   },
   post (url, data, options = {}) {
     var headers = options.headers || {}
-    if (api.URL(url).includes(apiBaseUrl)) headers['jax-client-token'] = clientToken
-    return axios.post(api.URL(url), data, { headers })
+    return httpRequest(api.URL(url), {
+      body: data instanceof FormData ? data : JSON.stringify(data),
+      headers
+    })
   }
+}
+
+function httpRequest (url, config = {}) {
+  console.log(url)
+  let requestOptions = {
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'omit',
+    method: config.method || 'GET',
+    headers: {
+      Origin: url.origin,
+      ...config.headers
+    }
+  }
+  // if (url.href.includes(apiBaseUrl)) requestOptions.headers['jax-client-token'] = clientToken
+
+  if (!(config.body instanceof FormData)) {
+    requestOptions.headers['Content-Type'] = 'application/json'
+  }
+
+  if (config.body) requestOptions.body = config.body
+
+  console.log(requestOptions)
+
+  return fetch(url, requestOptions)
 }
 
 export default api
