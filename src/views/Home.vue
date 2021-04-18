d9becebce239b1071459ef630333e81953d8cf29
WhiskeeDev <email@liamholmes.co.uk>
2021-04-18 19:00:17 +0100

    Add message about current development to the homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,5 +3,30 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
       <h1 class="h3 mb-0 text-gray-800">My Home</h1>
     </div>
+    <div class="row justify-content-center">
+      <div class="col-6 text-justify">
+        <p>
+          <em>Hello! This page is new, and is still under development, meaning it's not quite ready yet!</em>
+        </p>
+        <p>
+          <em>Hopefully in the future, your homepage will include lots of useful information, specifically catered for you.</em>
+        </p>
+        <p>
+          <em>But, until then, you can have this cookie instead:</em>
+        </p>
+        <h1 class="text-center cookie"><i class="fad fa-cookie-bite"></i></h1>
+        <p>
+          <em>p.s. sorry there is a bite taken out of it, I was hungry...</em>
+        </p>
+        <p>
+          <em>~The JaxBot Development Team</em>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
+<style lang="stylus" scoped>
+.cookie
+  color #c3854c
+  font-size 5em
+</style>
